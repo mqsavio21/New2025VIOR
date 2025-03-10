8e067f869f5480b13623deac773ffffbf2ac8c18
Savio Qalby <152057506+mqsavio21@users.noreply.github.com>
2025-03-10 14:55:47 +0700

    d

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
         },
     },
     build: {
-        manifest: true,
+        manifest: 'manifest.json',
         outDir: 'public/build',
         rollupOptions: {
             output: {
